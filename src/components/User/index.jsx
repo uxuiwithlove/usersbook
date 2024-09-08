06d0717b694f6c2ceff8a3dcc19e460fdf1abda2
uxuiwithlove <veda3000@gmail.com>
2024-09-08 22:34:16 +0300

    123

--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -3,15 +3,29 @@ import "./User.scss";
 const User = (props) => {
   return (
     <div className="user-card">
-      <div className="user-avatar">
-        <img src={props.picture} alt="User Avatar" />
+      <div className="user-card-title">
+        <div className="user-avatar">
+          <img src={props.picture} alt="User Avatar" />
+        </div>
+        <div className="user-main">
+          <h2 className="user-name">{props.name}</h2>
+          <p className="user-email">{props.email}</p>
+        </div>
+        <button className="follow-button">D</button>
       </div>
       <div className="user-info">
-        <h2 className="user-name">{props.name}</h2>
-        <p className="user-email">{props.email}</p>
-        <p className="user-phone">{props.phone}</p>
-        <p className="user-location">{props.location}</p>
-        <button className="follow-button">Delete</button>
+        <div className="user-info__row">
+          <p className="subtitle">Phone No</p>
+          <p className="user-phone">{props.phone}</p>
+        </div>
+        <div className="user-info__row">
+          <p className="subtitle">Birthday</p>
+          <p className="user-location">{props.dobdate}</p>
+        </div>
+        <div className="user-info__row">
+          <p className="subtitle">Address</p>
+          <p className="user-location">{props.location}</p>
+        </div>
       </div>
     </div>
   );
