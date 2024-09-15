4ecbe08760d4e078ac36c39564e4c8625f8e45b5
uxuiwithlove <veda3000@gmail.com>
2024-09-15 17:35:46 +0300

    Search users done

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -5,6 +5,7 @@ import "./Users.scss";
 
 const Users = () => {
   const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(""); // Для хранения поискового запроса
 
   // Функция для получения данных пользователей
   const getUsers = async () => {
@@ -23,6 +24,30 @@ const Users = () => {
     setUsers((prevUsers) => prevUsers.filter((user) => user.email !== email));
   };
 
+  // Обработчик изменения поискового запроса
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value.toLowerCase()); // Преобразуем введенный текст к нижнему регистру
+  };
+
+  // Фильтрация пользователей на основе поискового запроса
+  const filteredUsers = users.filter((user) => {
+    const fullName = `${user.name.first} ${user.name.last}`.toLowerCase();
+    const email = user.email.toLowerCase();
+    const phone = user.phone.toLowerCase();
+    const dobdate = new Date(user.dob.date).toLocaleDateString().toLowerCase();
+    const location =
+      `${user.location.city}, ${user.location.country}`.toLowerCase();
+
+    // Проверяем, содержит ли любое из полей введенный поисковый запрос
+    return (
+      fullName.includes(searchTerm) ||
+      email.includes(searchTerm) ||
+      phone.includes(searchTerm) ||
+      dobdate.includes(searchTerm) ||
+      location.includes(searchTerm)
+    );
+  });
+
   // Запрос выполняется при монтировании компонента
   useEffect(() => {
     getUsers();
@@ -35,20 +60,22 @@ const Users = () => {
           type="text"
           placeholder="Search users..."
           className="search-input"
+          value={searchTerm}
+          onChange={handleSearch} // Обработчик изменения поискового запроса
         />
         <button onClick={getUsers} className="refresh-button">
           Refresh Users
         </button>
       </div>
       <div className="container">
-        {users.map((user, index) => (
+        {filteredUsers.map((user, index) => (
           <User
             key={index}
             name={`${user.name.first} ${user.name.last}`}
             email={user.email}
             phone={user.phone}
             picture={user.picture.medium}
-            dobdate={user.dob.date}
+            dobdate={new Date(user.dob.date).toLocaleDateString()}
             location={`${user.location.city}, ${user.location.country}`}
             onDelete={handleDeleteUser} // Передаем функцию удаления
           />
